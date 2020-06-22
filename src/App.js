5b927b57c8a97e7ab27619e1fe63496d339eed25
Lucca Scarano <lucca.scarano@hotmail.com>
2020-06-22 19:39:58 -0300

    frontend application of repository reader - Possible to create and delete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+  //**repositories** points to the actual state;
+  //**setRepositories** creates a completely new state and aggregate it in this const
+  //**useState** sets our inicial state;
+  useEffect(()=>{
+    api.get('/repositories')
+      .then(response => {
+        setRepositories(response.data);
+      })
+  })
+
+
   async function handleAddRepository() {
-    // TODO
+    const async_create = await api.post('/repositories', {
+      title: `GoStack with ReactJS ${Date.now()}`,
+      url: `https://${Date.now()}.com`,
+      techs: [
+        "React",
+        "React Native",
+        "Node.js"
+      ]
+    });
+
+    const repository = async_create.data;
+    setRepositories([...repositories, repository])
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  async function handleRemoveRepository(repositoryId) {
+    const async_delete = await api.delete(`/repositories/${repositoryId}`);
   }
 
   return (
     <div>
+      <h1>Repositories</h1>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => <li key = {repository.id}>{repository.title}
+        <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button></li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
-
-export default App;
