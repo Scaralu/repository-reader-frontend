43f5904a986e35d7649a699751da94f3d3ca11f5
Lucca Scarano <lucca.scarano@hotmail.com>
2020-06-22 22:43:10 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,61 +1,42 @@
-import React, {useState, useEffect} from "react";
-
-import api from './services/api';
-
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-
-function App() {
+import api from "./services/api";
+export default function App() {
   const [repositories, setRepositories] = useState([]);
-
-  useEffect(() => {
-    api.get('repositories').then(response => {
+  //**repositories** points to the actual state;
+  //**setRepositories** creates a completely new state and aggregate it in this const
+  //**useState** sets our inicial state;
+  useEffect(()=>{
+    api.get('/repositories').then(response => {
       setRepositories(response.data);
-    });
-}, []);
+    })
+  }, []);
 
   async function handleAddRepository() {
-    const response = await api.post('repositories', {
-      title: `Novo Repositório ${Date.now()}`,
-      url:  `https://github.com/scaralu/${Date.now()}`,
+    const async_create = await api.post('/repositories', {
+      title: `GoStack with ReactJS ${Date.now()}`,
+      url: 'https://alou.com',
       techs: [
-        "NodeJS",
-        "ReactJS",
-        "FullStack",
-      ],
+        "React",
+        "React Native",
+        "Node.js"
+      ]
     });
-
-    const repository = response.data;
-
+    const repository = async_create.data;
     setRepositories([...repositories, repository]);
   }
-
-  async function handleRemoveRepository(id) {
-    await api.delete(`repositories/${id}`, {});
-    setRepositories(repositories.filter(repository => repository.id !== id));
+  async function handleRemoveRepository(repositoryId) {
+      await api.delete(`/repositories/${repositoryId}`);
+      setRepositories(repositories.filter(repository => repository.id !== repositoryId))
   }
-
   return (
     <div>
+      <h1>Repositories</h1>
       <ul data-testid="repository-list">
-
-      {repositories.map(repository => (
-        <li key={repository.id}>
-          {repository.title} 
-          <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
-        </li>
-      ))}
-
-        {/* <li>
-          Repositório 1
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li> */}
+        {repositories && repositories.map(repository => <li key = {repository.id}>{repository.title}
+        <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button></li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
-
-export default App;
